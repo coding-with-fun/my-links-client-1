90f771fa64c7dafe4b95ec0d9c8e6bcf4e15f8ca
Harrsh Patel <me.harrsh.patel@gmail.com>
2021-10-17 17:31:49 +0530

    Added conditional links list

--- src/components/LinksList.js
+++ src/components/LinksList.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { UserContext } from "../contexts/UserContext";
 
 const LinksList = ({ links, handleList }) => {
+    const { isUserAuthenticated } = useContext(UserContext);
+
     const handleDraggedList = (result) => {
         if (!result.destination) return;
 
@@ -12,7 +15,7 @@ const LinksList = ({ links, handleList }) => {
         handleList(items);
     };
 
-    return (
+    return !isUserAuthenticated ? (
         <div className="links__container">
             <DragDropContext onDragEnd={handleDraggedList}>
                 <Droppable droppableId="links">
@@ -30,13 +33,22 @@ const LinksList = ({ links, handleList }) => {
                                         index={index}
                                     >
                                         {(provided) => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                                className="link_details"
-                                            >
-                                                <p>{title}</p>
+                                            <div className="link_container">
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    className="link_details"
+                                                >
+                                                    <p>{title}</p>
+                                                </div>
+
+                                                <i
+                                                    className="bi bi-info-circle-fill info_icon"
+                                                    onClick={() =>
+                                                        console.log("object")
+                                                    }
+                                                ></i>
                                             </div>
                                         )}
                                     </Draggable>
@@ -48,6 +60,20 @@ const LinksList = ({ links, handleList }) => {
                     )}
                 </Droppable>
             </DragDropContext>
+        </div>
+    ) : (
+        <div className="links__container">
+            <div className="links_list">
+                {links.map(({ id, title, url }) => {
+                    return (
+                        <div className="link_details" key={id}>
+                            <a href={url} target="_blank" rel="noreferrer">
+                                {title}
+                            </a>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
